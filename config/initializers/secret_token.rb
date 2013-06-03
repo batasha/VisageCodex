f1273bfcfd13735cc83f669b2d45b23569b9853b
Natasha Badillo <natasha.badillo@gmail.com>
2013-06-03 10:26:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisageCodex::Application.config.secret_token = '06719fd15f81c1971d7bd94f73ea4ba9897782f45cd1627bafe730f1a694bd5203b723356898c0adb268f84657b957a09d4b581ed8657f5e9d2e0092a6e24711'
