c893446ac862fcf4479f23ea9acdd142261bfd70
Natasha Badillo <natasha.badillo@gmail.com>
2013-06-07 16:36:38 -0400

    fix production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ VisageCodex::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'batasha-prod',
+      :access_key_id => ACCESS,
+      :secret_access_key => SECRET
+    }
+  }
 end
